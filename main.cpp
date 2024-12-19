6c8648632b0313099b19033344350c6cdb2b3c4d
MasterShifuCsgo <kasparbergert6@gmail.com>
2024-12-20 00:38:20 +0200

    now shows the user all .txt files when loading.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
-#include <array>
+#include <array> // for std::array
+#include <filesystem>
 #include <fstream>
-#include <functional>
+#include <functional> // for std::function
 #include <iostream>
-#include <limits>
+#include <limits> // for numeric_limits
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -12,6 +13,8 @@ class editor {
 
 public:
   std::vector<std::string> endresult{}; // holds data for each line seperately.
+  std::string LoadedFromFile =
+      "000.txt"; // remains 000.txt if nothing is loaded;
   editor(std::vector<std::string> dummydata) : endresult(dummydata) {}
   editor() {};
 
@@ -20,11 +23,10 @@ public:
       std::cout << "nothing to print...\n";
     } else {
       for (int i = 0; i < endresult.size(); i++) {
-        std::cout << i << ": " << endresult[i] << '\n';
+        std::cout << i << ": " << endresult[i] << std::endl;
       }
     }
   }
-
   void add(const std::string &item, int &line) {
 
     if (item.size() <= 1) {
@@ -77,6 +79,7 @@ void switchLocations(editor &ed);
 void saveToFile(editor &ed);
 void loadFromFile(editor &ed);
 
+// initalizing area
 std::unordered_map<int, std::function<void(editor &)>> commands = {
     {0, addItem},         {1, removeItem}, {2, replaceItem},
     {3, switchLocations}, {4, saveToFile}, {5, loadFromFile}};
@@ -84,7 +87,6 @@ std::unordered_map<int, std::function<void(editor &)>> commands = {
 constexpr std::array<const char *, 6> actions = {
     "ADD", "REMOVE", "REPLACE", "SWITCH LOCATIONS", "SAVE", "LOAD"};
 
-// initalizing
 // std::vector<std::string> dummydata{"data", "123", "here i am", "Johney"};
 editor ed = editor(/* dummydata */); // enter dummy data here.
 
@@ -92,10 +94,8 @@ int main() {
 
   // user terminal
   while (true) {
-
-    std::cout << "current working editor\n";
     std::cout << "--Start--\n";
-    // print the editor
+    // print the contents
     ed.view();
 
     std::cout << "--End--\n";
@@ -111,7 +111,7 @@ int main() {
     while (true) {
       std::cout << "Choose command (number): ";
 
-      // returns false if the input is invalid
+      // checking user input
       if (!(std::cin >> chosen_command)) {
         std::cin.clear(); // clear the flag
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(),
@@ -122,9 +122,11 @@ int main() {
       break; // input was valid
     }
 
+    // find the command and execute it.
     if (commands.find(chosen_command) != commands.end()) {
       commands[chosen_command](ed);
     } else {
+      system("clear");
       std::cout << "Invalid command.\n";
     }
   }
@@ -152,7 +154,6 @@ void addItem(editor &ed) {
   std::string item;
   std::cout << "What would you like to add: ";
   getline(std::cin, item);
-  std::cin.ignore();
   line_num = getValidatedLineNumber();
   ed.add(item, line_num);
 }
@@ -160,6 +161,7 @@ void removeItem(editor &ed) {
   int line_num;
   std::cout << "Enter the line you would like to remove: ";
   line_num = getValidatedLineNumber();
+  system("clear");
   ed.remove(line_num);
 }
 void replaceItem(editor &ed) {
@@ -171,6 +173,7 @@ void replaceItem(editor &ed) {
   std::cin.ignore();
   std::cout << "\nEnter the item you want to replace it with: ";
   getline(std::cin, item);
+  system("clear");
   ed.replace(line_num, item);
 }
 void switchLocations(editor &ed) {
@@ -179,23 +182,28 @@ void switchLocations(editor &ed) {
   line_num1 = getValidatedLineNumber();
   std::cout << "\nEnter line 2 location: ";
   line_num2 = getValidatedLineNumber();
+  system("clear");
   ed.switch_locations(line_num1, line_num2);
 }
 void saveToFile(editor &ed) {
-  std::string filename;
-
+  std::string fileName;
   while (true) {
     std::cin.ignore();
+    // check if user has loaded a file alreay.
+    if (ed.LoadedFromFile != "000.txt") {
+      std::cout << "You loaded from file: " << ed.LoadedFromFile << '\n';
+    }
     std::cout << "Enter the file name you would like to save to: ";
-    getline(std::cin, filename);
 
-    if (filename.size() < 4 && filename.substr(filename.size() - 4) != ".txt") {
+    getline(std::cin, fileName);
+
+    if (fileName.size() < 4 && fileName.substr(fileName.size() - 4) != ".txt") {
       std::cout << "Please enter a filename with \'.txt\' at the end.";
       continue;
     }
   }
 
-  std::ofstream saveFile(filename, std::ios::out);
+  std::ofstream saveFile(fileName, std::ios::out);
   if (!saveFile.is_open()) {
     std::cout << "Error: Unable to open file for saving.\n";
     return;
@@ -203,9 +211,35 @@ void saveToFile(editor &ed) {
   for (unsigned int i = 0; i < ed.endresult.size(); i++) {
     saveFile << i << ". " << ed.endresult[i] << '\n';
   }
+  system("clear");
   std::cout << "Save Completed\n";
 }
 void loadFromFile(editor &ed) {
+
+  system("clear");
+  // printing all the current .txt files in the current directory
+  std::filesystem::path working_directory = ".";
+  std::string fileName;
+  try {
+    int i = 1;
+
+    std::cout << "all .txt files in the directory\n";
+    for (const auto &entry :
+         std::filesystem::directory_iterator(working_directory)) {
+
+      std::string fileName = entry.path().filename().string();
+      if (entry.is_regular_file() &&
+          fileName.substr(fileName.size() - 4) == ".txt") {
+        std::cout << i << ". " << fileName << '\n';
+        i++;
+      }
+    }
+  } catch (const std::filesystem::filesystem_error &e) {
+    std::cerr << "Filesystem Error: " << e.what() << std::endl;
+  } catch (const std::exception &e) {
+    std::cerr << "General exception: " << e.what() << std::endl;
+  }
+
   while (true) {
     std::cin.ignore();
     std::string filename;
@@ -229,6 +263,9 @@ void loadFromFile(editor &ed) {
     while (getline(loadFile, line)) {
       ed.endresult.push_back(line);
     }
+
+    ed.LoadedFromFile = filename; // already appends .txt
+    system("clear");
     std::cout << "Completed Loading.\n";
     break;
   }
